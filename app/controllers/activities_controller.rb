2edd51c01c47fd11d67acb236b75bb81b03ce31e
Edgar Rodriguez <edgar.rodriguez.turrubiates@gmail.com>
2019-06-19 19:23:48 -0500

    Can create new activities and see them in a list

---
+++ app/controllers/activities_controller.rb
@@ -0,0 +1,23 @@
+class ActivitiesController < ApplicationController
+	def index
+		@activities = Activity.all
+	end
+	def show
+		@activity = Activity.find(params[:id])
+	end
+
+	def new
+	end
+
+	def create
+		@activity = Activity.new(activity_params)
+
+		@activity.save
+		redirect_to @activity
+	end
+
+	private
+		def activity_params
+			params.require(:activity).permit(:name, :text)
+		end
+end
